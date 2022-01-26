7d6f4b7268e9dc5ddbd25a1691a9f10539053ae0
Avidor-Schwartzman <97688327+Avidor-Schwartzman@users.noreply.github.com>
2022-01-26 16:35:39 +0200

    bug fixed- dealing with multiple ways to fix the bug of dealing with upper cases and lower cases
    in this assignment ( the first one) we have found the way to deal with the bug of the caps and lowercases letters. we have found that in order that the programm will work as it should I had to write the three types of writings that I`ve wanted to check. Lowercases, capital letters and Upper cases. and then I`ve written the commands of IF and ELSE about those three conditions and finally we got the output of true\false according to the way we have written it

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ metal indicator program
 
 ## Project plans
 
-- bug: deal with uppercase, lowercase, etc. in input: "Tool" should say True, not False
+- bug: deal with uppercase, lowercase, etc. in input: "Tool" should say True, not False- Checked- V
 - feature: add more bands
 - feature: when the band is not metal, write False but when the indicator doesn't know the band write "Don't know"
 - feature: Add emojis to output
--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 print ("--------")
 band_name = input ("please enter a band name")
 print (band_name)
+
+#band_name = band_name.lower()
+#print (band_name)
+
 print ("--------")
 # this won't work:
-# metal_bands = ["amon amarth, bring me the horizon, tool,"]
+# metal_bands = ["amon amarth, bring me the horizon, Tool,"]
 # but this will:
-metal_bands = ["amon amarth", "bring me the horizon", "tool"]
-if band_name in metal_bands:  
+metal_bands_lowercase = ["amon amarth", "bring me the horizon", "tool" , "metallica", "megadeth", "iron maiden" , "motorhead"]
+metal_bands_uppercase = ["AMON AMARTH", "BRING ME THE HORIZON" , "TOOL" , "METALLICA", "MEGADETH" , "IRON MAIDEN", "MOTORHEAD"]
+metal_bands_title = ["Amon Amarth", "Bring Me The Horizon", "Tool", "Metallica", "Megadeth" , "Iron Maiden" , "Motorhead"]
+if band_name in metal_bands_lowercase:    
     print("True") 
 else: 
-    print("fales")    
+    print("false")   
+if band_name in metal_bands_uppercase:  
+    print("true")  
+else:           
+    print ("false")    
+if band_name in metal_bands_title: 
+    print ("true")
+else:
+    print("false")    
+# if the band isnt written in the direcory write: Don`t know
+if (band_name in metal_bands_lowercase) or (band_name in metal_bands_uppercase) or (band_name in metal_bands_title)
+    print ("true")
+else: 
+    print ("don`t know")
